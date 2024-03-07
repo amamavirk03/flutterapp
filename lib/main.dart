0ac52b5e82e53210a0a363367d80fea52a51224d
amama virk <amamavirk@gmail.com>
2024-03-07 21:27:00 +0500

    watsapp appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firstapp/buildingappscreen.dart';
 import 'package:firstapp/practice.dart';
+import 'package:firstapp/watsappapp.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BuildingAppScreen(),
+      home: const WatsappApp(),
     );
   }
 }
---
+++ lib/watsappapp.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WatsappApp extends StatefulWidget {
+  const WatsappApp({super.key});
+
+  @override
+  State<WatsappApp> createState() => _WatsappAppState();
+}
+
+class _WatsappAppState extends State<WatsappApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(
+          0xff008069,
+        ),
+        actions: [
+          Icon(
+            Icons.camera_alt_outlined,
+            size: 25,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Icon(
+            Icons.search,
+            size: 25,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Icon(
+            Icons.drag_indicator,
+            size: 25,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+        ],
+        title: Text(
+          "Watsappp",
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
